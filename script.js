5b01406b70727787b2be9459e85d1c7dfba10be4
QueenJammie <jammie.lacc@outlook.com>
2022-09-08 21:20:57 -0400

    Import my project to github

---
+++ script.js
@@ -0,0 +1,37 @@
+function deLavoisier()
+{
+  alert("Antoine Laurent Lavoisier");
+}
+function avogadro()
+{
+  alert("Amedeo Avogadro");
+}
+function pasteur()
+{
+  alert("Louis Pasteur");
+}
+function nobel()
+{
+  alert("Alfred Bernhard Nobel");
+}
+function mendeleïeve()
+{
+  alert("Dmitri Ivanovitch Mendeleïev");
+}
+function curie()
+{
+  alert("Marie Curie");
+}
+
+let alertDeLavoisier = document.querySelector("#deLavoisier");
+alertDeLavoisier.addEventListener("click", deLavoisier);
+let alertAvogadro = document.querySelector("#avogadro");
+alertAvogadro.addEventListener("click", avogadro);
+let alertPasteur = document.querySelector("#pasteur");
+alertPasteur.addEventListener("click", pasteur);
+let alertNobel = document.querySelector("#nobel");
+alertNobel.addEventListener("click", nobel);
+let alertMendeleïeve = document.querySelector("#mendeleïeve");
+alertMendeleïeve.addEventListener("click", mendeleïeve);
+let alertCurie = document.querySelector("#curie");
+alertCurie.addEventListener("click", curie);
